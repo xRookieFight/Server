39a807f9a288f6c20c517a2c6d8606510c291bca
metehngzr <ozgurmete123@outlook.com>
2020-07-25 02:42:40 +0300

    second commit

--- src/main/kotlin/net/prismeon/prismeonserver/PrismeonServer.kt
+++ src/main/kotlin/net/prismeon/prismeonserver/PrismeonServer.kt
@@ -4,10 +4,24 @@ class PrismeonServer {
     companion object {
         var instance: PrismeonServer? = null
             set(value){
-                if (field != null){
-                    throw UnsupportedOperationException("Cannot init server twice!")
-                }
+                if (field != null) throw UnsupportedOperationException("Cannot init server twice!")
                 field = value
             }
+    }
+
+    var running = true
+    var stopped = false
+
+    fun start() {
+        // Initalize
+        try {
+            while (running){
+                // Server Loop
+            }
+        } catch (exception: Exception){
+            // TODO Saving Worlds/Players, Disabling Plugins.
+        } finally {
+            stopped = true
+        }
     }
 }
--- src/test/kotlin/net/prismeon/PrismeonTest.kt
+++ src/test/kotlin/net/prismeon/PrismeonTest.kt
@@ -1,8 +1,11 @@
 package net.prismeon
 
+import net.prismeon.prismeonserver.PrismeonServer
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class PrismeonTest {
-
+    fun a(): Unit {
+        PrismeonServer.instance!!.start()
+    }
 }
